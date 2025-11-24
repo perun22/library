76fabfc6ab0efd06354619f6690e5820b91f20f1
Suzana <100297163+perun22@users.noreply.github.com>
2025-11-24 13:44:05 +0100

    Create book function

---
+++ script.js
@@ -0,0 +1,21 @@
+modal.addEventListener("click", e => {
+    const dialogDimensions = modal.getBoundingClientRect();
+    if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+    ) {
+        modal.close();
+    }
+});
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+

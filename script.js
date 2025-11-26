2cffd6274afad372c07b1c2ccc5f938cc24df59e
Suzana <100297163+perun22@users.noreply.github.com>
2025-11-26 18:31:22 +0100

    Add modal functionality

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-modal.addEventListener("click", e => {
-    const dialogDimensions = modal.getBoundingClientRect();
+const bookDialog = document.querySelector("dialog");
+const openBtn = document.getElementById("modal-btn-open");
+const closeBtn = document.getElementById("close-btn");
+
+openBtn.addEventListener("click", () => {
+    bookDialog.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+    bookDialog.close();
+});
+
+dialog.addEventListener("click", (e) => {
+    const dialogDimensions = dialog.getBoundingClientRect();
     if (
-    e.clientX < dialogDimensions.left ||
-    e.clientX > dialogDimensions.right ||
-    e.clientY < dialogDimensions.top ||
-    e.clientY > dialogDimensions.bottom
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
     ) {
-        modal.close();
+        dialog.close();
     }
 });
 
@@ -17,5 +29,5 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.ID = crypto.randomUUID();
 }
-

7135ee1a8d5aa383b0ce506915907d8455a6b088
Suzana <100297163+perun22@users.noreply.github.com>
2025-11-27 20:26:55 +0100

    Change minor details

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bookDialog = document.querySelector("dialog");
 const openBtn = document.getElementById("modal-btn-open");
 const closeBtn = document.getElementById("close-btn");
+const modalForm = document.getElementById("modal-form");
+const submitBtn = document.getElementById("submit");
 
 openBtn.addEventListener("click", () => {
     bookDialog.showModal();
@@ -10,24 +12,35 @@ closeBtn.addEventListener("click", () => {
     bookDialog.close();
 });
 
-dialog.addEventListener("click", (e) => {
-    const dialogDimensions = dialog.getBoundingClientRect();
+bookDialog.addEventListener("click", (e) => {
+    const dialogDimensions = bookDialog.getBoundingClientRect();
     if (
         e.clientX < dialogDimensions.left ||
         e.clientX > dialogDimensions.right ||
         e.clientY < dialogDimensions.top ||
         e.clientY > dialogDimensions.bottom
     ) {
-        dialog.close();
+        bookDialog.close();
     }
 });
 
-const myLibrary = [];
-
-function Book(title, author, pages, read) {
+function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.readStatus = readStatus;
     this.ID = crypto.randomUUID();
 }
+const myLibrary = [];
+
+function addBookToLibrary() {}
+
+function getFormData(event) {
+    const userData = new FormData(event.target);
+    const bookData = {};
+    userData.forEach((value, key) => (bookData[key] = value));
+    event.preventDefault();
+    bookDialog.close();
+    console.log(myLibrary);
+}
+modalForm.addEventListener("submit", getFormData);
